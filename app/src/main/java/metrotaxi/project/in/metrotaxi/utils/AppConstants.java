8be8cfefa25002b87ee9443d022b46e99bc588af
rimon <rimonrpallan@gmail.com>
2017-10-30 04:11:49 +0530

    Validation Updates One

--- app/src/main/java/metrotaxi/project/in/metrotaxi/utils/AppConstants.java
+++ app/src/main/java/metrotaxi/project/in/metrotaxi/utils/AppConstants.java
@@ -5,6 +5,6 @@ public class AppConstants {
     public static final String USER_NAME = "user_name";
 
     public static final String FCM_ID = "fcm_id";
-    public static final String URL = "http://192.168.43.171:8084/MetroTaxi/";
+    public static final String URL = "http://192.168.1.36:8080/MetroTaxi/";
     public static final String USER_TYPE = "user_type";
 }
--- app/src/main/java/metrotaxi/project/in/metrotaxi/webservices/RetrofitClient.java
+++ app/src/main/java/metrotaxi/project/in/metrotaxi/webservices/RetrofitClient.java
@@ -12,7 +12,7 @@ public class RetrofitClient {
     private static OkHttpClient client = new OkHttpClient.Builder().connectTimeout(300, TimeUnit.SECONDS).readTimeout(300, TimeUnit.SECONDS).build();
     public static Retrofit getRetrofitClient() {
         if (retrofit == null) {
-            retrofit = new Retrofit.Builder().baseUrl("http://192.168.43.171:8084/MetroTaxi/")
+            retrofit = new Retrofit.Builder().baseUrl("http://192.168.1.36:8080/MetroTaxi/")
                     .client(client)
                     .addConverterFactory(GsonConverterFactory.create()).build();
         }
